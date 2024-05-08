642dd90c1233fe6d65ffa679d8a60333efc205b1
NathanDSanta <natan.d.santa@gmail.com>
2024-05-08 23:58:25 +0200

    more testing

--- pruebas/main.cpp
+++ pruebas/main.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
 using namespace std;
 
+void print(int arg) {
+  cout << arg << endl;
+  cout << "Hello, World!" << endl;
+}
+
 int main() {
-  cout << "Hello, world!" << endl;
+  cout << "Hello, World!" << endl;
+  print(1);
+  int i = 0;
+  while (i < 10) {
+    cout << i << endl;
+    i++;
+  }
+
   return 0;
 }
